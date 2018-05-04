c152c0d9940a30a5cf44827687cb74cedca0edcc
Surya RajaSagi <suryarajasagi@gmail.com>
2018-05-04 23:43:04 +0530

    Add files via upload

---
+++ Mobile No. Verify.py
@@ -0,0 +1,8 @@
+# Accept a mobile number and check whether it is valid..
+
+number = str(input("Enter a number: "))
+
+if (len(number) == 10):
+    print("It's a valid mobile number..")
+else:
+    print("It's not a valid mobile number..")
