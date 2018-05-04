6c3a4454352b2d792e2c90b7e724d740d335d7f4
Surya RajaSagi <suryarajasagi@gmail.com>
2018-05-04 23:17:08 +0530

    Add files via upload

---
+++ Seperating Words.py
@@ -0,0 +1,10 @@
+# Accept a sentence and display each word seperately with it's length
+
+string = input("Enter a sentence: ")
+length = len(string)
+j = 0
+
+brk_str = string.split()
+
+for i in range(0,len(brk_str)):
+    print(brk_str[i],len(brk_str[i]))
