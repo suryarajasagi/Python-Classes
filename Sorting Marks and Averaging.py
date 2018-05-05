b190ba8058c1e4c79ebcdb527a771eab8986ce91
Surya RajaSagi <suryarajasagi@gmail.com>
2018-05-05 23:31:46 +0530

    Add files via upload

---
+++ Sorting Marks and Averaging.py
@@ -0,0 +1,30 @@
+# Accept marks from students until there's a '-1'. Print them in ascending order.
+# Get the count of no. of students whose marks are above the average.
+
+marks = []
+sorted_marks = []
+i = 0
+cond = True
+sum = 0
+
+while cond:
+    number = int(input("Enter the marks: "))
+    if number == -1:
+        break
+    sum += number
+    marks.insert(i, number)
+    i += 1
+
+sorted_marks = sorted(marks)
+print(sorted_marks)
+
+average = sum / i + 1
+print(average)
+count = 0
+
+for j in marks:
+    k = 0
+    if marks[k] > average:
+        count += 1
+    k += 1
+print(count)
